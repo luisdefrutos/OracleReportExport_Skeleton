4da94e9abd51e2e4c72bf18b620466c000600322
luisdefrutos <luisdefrutos@outlook.com>
2025-11-20 00:53:23 +0100

    Archivos añadidos

---
+++ src/OracleReportExport.Domain/Models/IntCodeItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleReportExport.Domain.Models
+{
+    public class IntCodeItem
+    {
+        public int Key { get; set; }      // 0 ó 1
+        public string? Value { get; set; } // "S" ó "N"
+    }
+
+}
---
+++ src/OracleReportExport.Domain/Models/TableMasterParameterDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OracleReportExport.Domain.Models
+{
+    public class TableMasterParameterDefinition
+    {
+
+        public string? Name { get; set; }
+        public string? Label { get; set; }
+        public string? Type { get; set; }
+        public bool IsRequired { get; set; }
+        public string? Id { get; set; }
+        public string? Text { get; set; }
+        public List<string?>? ValuesRequired { get; set; }
+
+        public string? SqlQueryMaster { get; set; }
+    }
+ 
+}
